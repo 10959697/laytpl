e864c71126550a7acb37b427e152fe09f9dee950
sentsin <xu@sentsin.com>
2016-10-30 07:51:04 +0800

    1.2.0

---
+++ README.md
@@ -0,0 +1,25 @@
+
+## 介绍(Introduce)
+laytpl是一款非常轻量的JavaScript模板引擎，语法遵循原生JS。laytpl试图打造极简的模版渲染。
+
+
+## 模版语法
+
+输出一个普通字段，不转义html：
+```
+{{ d.field }}
+```
+输出一个普通字段，并转义html：
+```
+{{= d.field }}
+```
+逻辑处理： 
+```
+{{# JavaScript表达式 }}
+
+## 协议(License) 
+The MIT License
+
+## 详细文档
+[官网](http://www.layui.com/doc/modules/laytpl.html)
+
---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+﻿/**
+ laytpl构建
+*/
+
+var pkg = require('./package.json');
+
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var header = require('gulp-header');
+var del = require('del');
+
+gulp.task('clear', function(cb){ //清理
+  return del(['./build/*'], cb);
+});
+
+//全部
+gulp.task('default', ['clear'], function(){
+  return gulp.src('./src/laytpl.js').pipe(uglify())
+   .pipe(header('/*! <%= pkg.name %>-v<%= pkg.version %> <%= pkg.description %> <%= pkg.license %> License  By <%= pkg.homepage %> */\n ;', {pkg: pkg}))
+  .pipe(gulp.dest('./build'));
+});
+
+
+
+
+
+
